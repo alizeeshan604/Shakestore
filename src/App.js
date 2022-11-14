1202974e458d07f9298f444e85594e9294ab6097
alizeeshan604 <82883473+alizeeshan604@users.noreply.github.com>
2022-11-14 21:09:41 +0500

    mychanges

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './Header'; //Include Header
+import Footer from './Footer'; //Include Footer
+import Shop from './Shop';
+
+function App() {
+  return (
+    <div className="App">
+      
+       
+          <Header></Header>
+            <Shop></Shop>
+            
+          <Footer></Footer>
+        
+     
+    </div>
+  );
+}
+export default App;
